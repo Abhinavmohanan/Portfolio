a1a55e1a6a1995e6e6b8e0cbbb3eccbe8e491a55
Abhinav M M <97299219+Abhinavmohanan@users.noreply.github.com>
2024-07-20 12:17:10 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abhinavmohanan.github.io',
+  base: 'Portfolio',
   integrations: [react(), tailwind()],
   experimental: { assets: true },
 });
